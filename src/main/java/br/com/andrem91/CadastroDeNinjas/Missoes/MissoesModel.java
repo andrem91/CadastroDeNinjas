d6beb9abdc545f90a13d47fa6a695faaa1289804
André Marques <andrem166@gmail.com>
2025-09-28 08:26:45 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/br/com/andrem91/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/br/com/andrem91/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package br.com.andrem91.CadastroDeNinjas.Missoes;
 
 import br.com.andrem91.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -16,29 +22,5 @@ public class MissoesModel {
     private String dificuldade;
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nome, String dificuldade) {
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 
 }
--- src/main/java/br/com/andrem91/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/br/com/andrem91/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,9 +2,15 @@ package br.com.andrem91.CadastroDeNinjas.Ninjas;
 
 import br.com.andrem91.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_ninjas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -16,38 +22,5 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 
 }
