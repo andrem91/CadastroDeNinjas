c26a24975436094aa9638b87d44d233d44378f30
André Marques <andrem166@gmail.com>
2025-09-29 18:32:54 -0300

    fix: Implementa a interface de NinjaRepository corretamente

--- src/main/java/br/com/andrem91/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/br/com/andrem91/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package br.com.andrem91.CadastroDeNinjas.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
