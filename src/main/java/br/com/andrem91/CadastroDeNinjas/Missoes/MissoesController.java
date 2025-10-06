a6b0bdd7350febce509176d9c6d43298cf8210d2
André Marques <andrem166@gmail.com>
2025-10-06 16:52:24 -0300

    feat: Cria as rotas do controller com mock

--- src/main/java/br/com/andrem91/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/br/com/andrem91/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,29 @@
 package br.com.andrem91.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Missão listadas com sucesso";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão criada com sucesso";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletada com sucesso";
+    }
+
 }
