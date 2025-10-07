de03125cf2158366a2dc9c557c4c6bdbaae9334f
André Marques <andrem166@gmail.com>
2025-10-07 12:15:38 -0300

    feat: Adiciona a rota Delete do ninja

--- src/main/java/br/com/andrem91/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/br/com/andrem91/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -44,8 +44,8 @@ public class NinjaController {
     }
 
     // Deletar ninja (DELETE)
-    @DeleteMapping("/deletar")
-    public String deletarNinjaPorId() {
-        return "Ninja deletado";
+    @DeleteMapping("/deletar/{id}")
+    public void deletarNinjaPorId(@PathVariable Long id) {
+        ninjaService.deletarNinjaPorId(id);
     }
 }
